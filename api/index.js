63665e7e5144f3c48e8d4ca2811f538b1bc97882
piducancore:/# <hola@piducan.dev>
2021-09-27 10:17:36 -0300

    and more

--- api/index.js
+++ api/index.js
@@ -1,28 +1,32 @@
 const serp = require("serp");
 
-module.exports = async (_, res) => {
+async function googleCL(q) {
   try {
     const results = await serp.search({
       host: "google.cl",
       qs: {
-        q: "el nuevo método de los delincuentes",
+        q,
         cr: "countryCL",
       },
       num: 100,
     });
-    // const verses = results
-    //   .map(({ title }) =>
-    //     title
-    //       .toLowerCase()
-    //       .replace(/\ \-.*/, "")
-    //       .replace(/\ \|.*/, "")
-    //       .replace(/\ \..*/, "")
-    //   )
-    //   .filter((x) => x.includes("nuevo"))
-    //   .filter((x) => x.includes("método"));
-    res.send(results.map((x) => JSON.stringify(x)));
-  } catch (error) {
-    console.log(error);
-    res.send(error);
+    return results;
+  } catch (err) {
+    throw err;
   }
+}
+
+module.exports = async (_, res) => {
+  const results = await googleCL("el nuevo método de los delincuentes");
+  // const verses = results
+  //   .map(({ title }) =>
+  //     title
+  //       .toLowerCase()
+  //       .replace(/\ \-.*/, "")
+  //       .replace(/\ \|.*/, "")
+  //       .replace(/\ \..*/, "")
+  //   )
+  //   .filter((x) => x.includes("nuevo"))
+  //   .filter((x) => x.includes("método"));
+  res.send(results.map((x) => JSON.stringify(x)));
 };
