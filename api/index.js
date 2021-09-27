c0b965ae71dccc04cc796b90235e261a067cc84f
piducancore:/# <hola@piducan.dev>
2021-09-27 10:06:54 -0300

    debug

--- api/index.js
+++ api/index.js
@@ -1,26 +1,26 @@
-const serp = require("serp");
+// const serp = require("serp");
 
 module.exports = async (_, res) => {
   try {
-    const results = await serp.search({
-      host: "google.cl",
-      qs: {
-        q: "el nuevo método de los delincuentes",
-        cr: "countryCL",
-      },
-      num: 100,
-    });
-    const verses = results
-      .map(({ title }) =>
-        title
-          .toLowerCase()
-          .replace(/\ \-.*/, "")
-          .replace(/\ \|.*/, "")
-          .replace(/\ \..*/, "")
-      )
-      .filter((x) => x.includes("nuevo"))
-      .filter((x) => x.includes("método"));
-    res.send(verses);
+    // const results = await serp.search({
+    //   host: "google.cl",
+    //   qs: {
+    //     q: "el nuevo método de los delincuentes",
+    //     cr: "countryCL",
+    //   },
+    //   num: 100,
+    // });
+    // const verses = results
+    //   .map(({ title }) =>
+    //     title
+    //       .toLowerCase()
+    //       .replace(/\ \-.*/, "")
+    //       .replace(/\ \|.*/, "")
+    //       .replace(/\ \..*/, "")
+    //   )
+    //   .filter((x) => x.includes("nuevo"))
+    //   .filter((x) => x.includes("método"));
+    res.send(["hola", "mundo"]);
   } catch (error) {
     console.log(error);
     res.send(error);
