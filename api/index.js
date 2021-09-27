bc0f973347f310bfda975efac8eb46230f8b9dde
piducancore:/# <hola@piducan.dev>
2021-09-27 10:11:14 -0300

    moar debug

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
-// const serp = require("serp");
+const serp = require("serp");
 
 module.exports = async (_, res) => {
   try {
-    // const results = await serp.search({
-    //   host: "google.cl",
-    //   qs: {
-    //     q: "el nuevo método de los delincuentes",
-    //     cr: "countryCL",
-    //   },
-    //   num: 100,
-    // });
+    const results = await serp.search({
+      host: "google.cl",
+      qs: {
+        q: "el nuevo método de los delincuentes",
+        cr: "countryCL",
+      },
+      num: 100,
+    });
     // const verses = results
     //   .map(({ title }) =>
     //     title
@@ -20,7 +20,7 @@ module.exports = async (_, res) => {
     //   )
     //   .filter((x) => x.includes("nuevo"))
     //   .filter((x) => x.includes("método"));
-    res.send(["hola", "mundo"]);
+    res.send(results.map((x) => JSON.stringify(x)));
   } catch (error) {
     console.log(error);
     res.send(error);
