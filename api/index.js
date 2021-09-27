cfda15896fe5b85fb01ab4ef9d40d69b967cc538
piducancore:/# <hola@piducan.dev>
2021-09-26 22:50:22 -0300

    add catch block

--- api/index.js
+++ api/index.js
@@ -1,23 +1,28 @@
 const serp = require("serp");
 
 module.exports = async (_, res) => {
-  const results = await serp.search({
-    host: "google.cl",
-    qs: {
-      q: "el nuevo método de los delincuentes",
-      cr: "countryCL",
-    },
-    num: 100,
-  });
-  const verses = results
-    .map(({ title }) =>
-      title
-        .toLowerCase()
-        .replace(/\ \-.*/, "")
-        .replace(/\ \|.*/, "")
-        .replace(/\ \..*/, "")
-    )
-    .filter((x) => x.includes("nuevo"))
-    .filter((x) => x.includes("método"));
-  res.send(verses);
+  try {
+    const results = await serp.search({
+      host: "google.cl",
+      qs: {
+        q: "el nuevo método de los delincuentes",
+        cr: "countryCL",
+      },
+      num: 100,
+    });
+    const verses = results
+      .map(({ title }) =>
+        title
+          .toLowerCase()
+          .replace(/\ \-.*/, "")
+          .replace(/\ \|.*/, "")
+          .replace(/\ \..*/, "")
+      )
+      .filter((x) => x.includes("nuevo"))
+      .filter((x) => x.includes("método"));
+    res.send(verses);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 };
