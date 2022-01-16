e5fac146a4a7cf1f0fdddb737855fb8a3b61d091
Rodrigo Moreira <hola@piducan.dev>
2022-01-16 10:54:38 -0300

    ok clean up more

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ function cleanUp(text) {
     .toLowerCase()
     .replace(/^(\d{2}|\d{1})\ ([a-z]{4}|[a-z]{3})\ \d{4}\ ·\ /, "")
     .replace(/publicado:\ (\d{2}|\d{1})\ ([a-z]{4}|[a-z]{3})\ \d{4}$/, "")
-    .replace(/\.\.\.$/, "")
     .replace(/^\.\.\./, "")
     .replace(/\ \-\ .*/, "")
+    .replace(/\ \|\ .*/, "")
     .replace(/^\[[a-z]{3}\]/, "")
+    .replace(/\.\.\.$/, "")
     .trim();
 }
 
