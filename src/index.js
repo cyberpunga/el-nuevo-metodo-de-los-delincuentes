36a1b2545c4f90560e6b28da624bb8ca50fd8ea7
Rodrigo Moreira <hola@piducan.dev>
2022-01-16 10:46:25 -0300

    clean up results

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const { stringify } = require("querystring");
 const { JSDOM } = require("jsdom");
 
+function cleanUp(text) {
+  return text
+    .toLowerCase()
+    .replace(/^(\d{2}|\d{1})\ ([a-z]{4}|[a-z]{3})\ \d{4}\ ·\ /, "")
+    .replace(/publicado:\ (\d{2}|\d{1})\ ([a-z]{4}|[a-z]{3})\ \d{4}$/, "")
+    .replace(/\.\.\.$/, "")
+    .replace(/^\.\.\./, "")
+    .replace(/\ \-\ .*/, "")
+    .replace(/^\[[a-z]{3}\]/, "")
+    .trim();
+}
+
 async function versosDeGoogle(params) {
   const { window } = await JSDOM.fromURL(`https://www.google.com/search?${stringify(params)}`);
-  const results = [...window.document.querySelectorAll("#main h3")].map((result) => ({
-    title: result.textContent,
-    description: result.parentElement.parentElement.nextElementSibling.nextElementSibling?.textContent,
-    href: result.closest("a")?.href,
-  }));
+  const results = [...window.document.querySelectorAll("#main h3")].map((result) => {
+    let url = new URL(result.closest("a")?.href);
+    let href = new URLSearchParams(url.searchParams).get("q");
+    let title = cleanUp(result.textContent);
+    let description = cleanUp(result.parentElement.parentElement.nextElementSibling.nextElementSibling?.children[0].children[0].children[0].textContent);
+    return { href, title, description };
+  });
   return results;
 }
 
