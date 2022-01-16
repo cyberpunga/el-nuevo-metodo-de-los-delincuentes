ed2dfe5a8a469a8d1db0a31924b8276bb3c98bcd
Rodrigo Moreira <hola@piducan.dev>
2022-01-16 18:44:46 -0300

    bold on playing

--- src/templates/index.js
+++ src/templates/index.js
@@ -7,18 +7,16 @@ import "../styles.css";
 
 const chance = new Chance();
 
-function Voice({ item, delay, setPlaying }) {
-  const playVoice = () => {
-    const audio = new Howl({
-      src: [`${slugify(item.title, { lower: true, strict: true })}.mp3`],
-      volume: chance.floating({ min: 0.5, max: 1, fixed: 2 }),
-      rate: chance.floating({ min: 0.7, max: 1.5, fixed: 2 }),
-      onplay: () => setPlaying(true),
-      onend: () => setPlaying(false),
-    });
-    setTimeout(() => audio.play(), delay);
-  };
-  React.useEffect(() => playVoice());
+function Vox({ item, setPlaying }) {
+  const audio = new Howl({
+    src: [`${slugify(item.title, { lower: true, strict: true })}.mp3`],
+    volume: chance.floating({ min: 0.4, max: 1, fixed: 2 }),
+    rate: chance.floating({ min: 0.8, max: 1.6, fixed: 2 }),
+    onplay: () => /* console.log(`${item.title} started`) */ setPlaying(true),
+    onend: () => /* console.log(`${item.title} ended`) */ setPlaying(false),
+  });
+  const playVoice = () => setTimeout(() => audio.play(), item.delay);
+  React.useEffect(() => playVoice(), []);
   return null;
 }
 
@@ -26,7 +24,7 @@ function Text({ item }) {
   const [playing, setPlaying] = React.useState(false);
   return (
     <a target="_blank" rel="noopener noreferrer" href={item.href} style={{ fontWeight: playing ? "bold" : "inherit" }}>
-      {item.title}. <Voice item={item} delay={chance.integer({ min: 0, max: 1000 * 100 })} setPlaying={setPlaying} />
+      {item.title}. <Vox item={item} setPlaying={setPlaying} />
     </a>
   );
 }
@@ -59,7 +57,9 @@ export default function Index({ pageContext }) {
     <div>
       <p>
         {results.map((item, index) => (
-          <Text key={index} item={item} />
+          <React.Fragment key={index}>
+            <Text item={{ ...item, delay: (index + 1) * 2000 }} />
+          </React.Fragment>
         ))}
       </p>
       <BackgroundMusic />
