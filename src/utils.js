e28004e43109893149882fd7599ddb171a1561c6
Rodrigo Moreira <hola@piducan.dev>
2022-02-13 19:55:14 -0300

    no clean up, no description key

--- src/utils.js
+++ src/utils.js
@@ -3,27 +3,13 @@ const { stringify } = require("querystring");
 const fetch = require("node-fetch");
 const fs = require("fs");
 
-function cleanUp(text) {
-  return text
-    .toLowerCase()
-    .replace(/^(\d{2}|\d{1})\ ([a-z]{4}|[a-z]{3})\ \d{4}\ ·\ /, "")
-    .replace(/publicado:\ (\d{2}|\d{1})\ ([a-z]{4}|[a-z]{3})\ \d{4}$/, "")
-    .replace(/^\.\.\./, "")
-    .replace(/\ \-\ .*/, "")
-    .replace(/\ \|\ .*/, "")
-    .replace(/^\[[a-z]{3}\]/, "")
-    .replace(/\.\.\.$/, "")
-    .trim();
-}
-
 async function getResults(params) {
   const { window } = await JSDOM.fromURL(`https://www.google.com/search?${stringify(params)}`);
   const results = [...window.document.querySelectorAll("#main h3")].map((result) => {
     let url = new URL(result.closest("a")?.href);
     let href = new URLSearchParams(url.searchParams).get("q");
-    let title = cleanUp(result.textContent);
-    let description = cleanUp(result.parentElement.parentElement.nextElementSibling.nextElementSibling?.children[0].children[0].children[0].textContent);
-    return { href, title, description };
+    let title = result.textContent;
+    return { href, title };
   });
   return results;
 }
