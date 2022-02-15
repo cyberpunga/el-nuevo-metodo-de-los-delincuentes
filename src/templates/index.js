f5a79f17803a3b0cde58e9cf96a937f4cf9d965f
Rodrigo Moreira <hola@piducan.dev>
2022-02-15 15:46:36 -0300

    play audios when words are visible

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,6 +1,6 @@
+import React, { useRef, Suspense } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { ScrollControls, Scroll, useScroll, Text, PositionalAudio } from "@react-three/drei";
-import React, { /* useEffect, useState, */ useRef, Suspense } from "react";
+import { ScrollControls, Scroll, useScroll, Text, PositionalAudio, useIntersect, Loader } from "@react-three/drei";
 import * as THREE from "three";
 
 import oswald from "@fontsource/oswald/files/oswald-latin-700-normal.woff";
@@ -13,41 +13,22 @@ const pickRandomElement = (array) => array[Math.floor(Math.random() * array.leng
 const fonts = [oswald, lora, merriweather, montserrat];
 
 function Word({ children, ...props }) {
-  // const color = new THREE.Color();
-  const fontProps = {
-    letterSpacing: -0.05,
-    lineHeight: 1,
-    "material-toneMapped": false,
-  };
-  const ref = useRef();
-  // const [hovered, setHovered] = useState(false);
-  // const over = (e) => (e.stopPropagation(), setHovered(true));
-  // const out = () => setHovered(false);
-  // Change the mouse cursor on hover
-  // useEffect(() => {
-  //   if (hovered) document.body.style.cursor = "pointer";
-  //   return () => (document.body.style.cursor = "auto");
-  // }, [hovered]);
-  // Tie component to the render-loop
-  // useFrame(({ camera }) => {
-  //   // Make text face the camera
-  //   ref.current.quaternion.copy(camera.quaternion);
-  //   // Animate font color
-  //   ref.current.material.color.lerp(color.set(hovered ? "#fa2720" : "white"), 0.1);
-  // });
+  const fontProps = { letterSpacing: -0.05, lineHeight: 1, "material-toneMapped": false };
+  const sound = useRef();
+  const ref = useIntersect(() => {
+    if (!sound.current.isPlaying) {
+      sound.current.play();
+    }
+  });
   return (
-    <React.Fragment>
-      <Text
-        ref={ref}
-        // onPointerOver={over}
-        // onPointerOut={out}
-        // onClick={() => window.open(props.href, "_blank").focus()}
-        {...props}
-        {...fontProps}
-        children={children}
-      />
-      <PositionalAudio autoplay loop position={props.position} url={props.audio} />
-    </React.Fragment>
+    <group>
+      <Text {...props} {...fontProps} children={children} />
+      <mesh ref={ref} position={props.position}>
+        <planeBufferGeometry attach="geometry" args={[0, 0]} />
+        <meshBasicMaterial attach="material" color="white" />
+        <PositionalAudio ref={sound} autoplay={false} loop={false} url={props.audio} detune={Math.random()} />
+      </mesh>
+    </group>
   );
 }
 
@@ -57,30 +38,31 @@ function Content({ results }) {
   const { viewport } = useThree();
   useFrame((state, delta) => {
     ref.current.position.z = THREE.MathUtils.damp(ref.current.position.z, Math.max(0, data.delta * 1000), 4, delta);
+    state.camera.rotation.y = THREE.MathUtils.lerp(state.camera.rotation.y, state.mouse.x * -0.4, 0.1);
+    state.camera.rotation.x = THREE.MathUtils.lerp(state.camera.rotation.x, state.mouse.y * 0.4, 0.1);
   });
   return (
-    <group ref={ref}>
+    <group ref={ref} position={[0, 0, 8]}>
       {results.map((item, index) => {
-        console.log(item);
         const randomProps = {
           position: [
-            index === 0 ? 0 : THREE.MathUtils.randFloatSpread(4),
-            index === 0 ? 0 : -viewport.height * index * 0.25,
-            index === 0 ? -4 : THREE.MathUtils.randFloatSpread(4),
+            index === 0 ? 0 : THREE.MathUtils.randFloatSpread(8),
+            index === 0 ? 0 : -viewport.height * index * 0.5,
+            index === 0 ? -4 : THREE.MathUtils.randFloatSpread(8),
           ],
           font: index === 0 ? fonts[0] : pickRandomElement(fonts),
-          fontSize: index === 0 ? 4 : Math.random(),
+          fontSize: 1,
           maxWidth: index === 0 ? viewport.width : viewport.width * Math.random(),
         };
         return (
           <React.Fragment key={index}>
-            {index > results.length - 5 ? (
+            {index > results.length - 8 ? (
               <Word
                 audio={item.audio}
                 {...randomProps}
                 position={[
                   randomProps.position[0],
-                  randomProps.position[1] + viewport.height * results.length * 0.25,
+                  randomProps.position[1] + viewport.height * results.length * 0.5,
                   randomProps.position[2],
                 ]}
                 href={item.href}
@@ -91,13 +73,13 @@ function Content({ results }) {
             <Word audio={item.audio} {...randomProps} href={item.href}>
               {item.title}
             </Word>
-            {index < 5 ? (
+            {index < 8 ? (
               <Word
                 audio={item.audio}
                 {...randomProps}
                 position={[
                   randomProps.position[0],
-                  randomProps.position[1] - viewport.height * results.length * 0.25,
+                  randomProps.position[1] - viewport.height * results.length * 0.5,
                   randomProps.position[2],
                 ]}
                 href={item.href}
@@ -115,17 +97,20 @@ function Content({ results }) {
 export default function Index({ pageContext }) {
   const { results } = pageContext;
   return (
-    <Canvas style={{ position: "absolute", top: 0, left: 0, width: "100vw", height: "100vh" }}>
-      <ScrollControls infinite damping={4} pages={results.length / 4 + 1} distance={1.5}>
-        <Scroll>
-          <Suspense fallback={null}>
-            <Content results={results} />
-          </Suspense>
-        </Scroll>
-      </ScrollControls>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <color attach="background" args={["#e71d36"]} />
-    </Canvas>
+    <React.Fragment>
+      <Canvas style={{ position: "absolute", top: 0, left: 0, width: "100vw", height: "100vh" }}>
+        <ScrollControls infinite damping={4} pages={results.length / 2 + 1} distance={1.5}>
+          <Scroll>
+            <Suspense fallback={null}>
+              <Content results={results} />
+            </Suspense>
+          </Scroll>
+        </ScrollControls>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <color attach="background" args={["#e71d36"]} />
+      </Canvas>
+      <Loader />
+    </React.Fragment>
   );
 }
